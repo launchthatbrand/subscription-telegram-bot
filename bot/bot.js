39b3118ca60883b330bb3e10d4beff89e5f4a384
Glitch (hello-express) <none>
2022-08-24 02:08:33 +0000

    🍨🎻 Checkpoint ./bot/bot.js:21601577/327

---
+++ bot/bot.js
@@ -0,0 +1,9 @@
+const { Telegraf } = require('telegraf');
+
+var bot = new Telegraf('5352985079:AAHd8LSeiKMTEmGG8r0uF2uqcCQBWK7HzSk') // We saved our bot token to the bot variable 
+
+bot.start(ctx => ctx.reply(`
+   Hi, I'm a simple bot
+`))
+
+bot.launch();
