1d28318cad02400d934bf44d8a3d2987f68dee67
Glitch (hello-express) <none>
2022-08-24 02:20:31 +0000

    🚕🎙 Checkpoint ./pages/api/bot.js:21601577/333

--- pages/api/bot.js
+++ pages/api/bot.js
@@ -4,6 +4,7 @@ var bot = new Telegraf('5352985079:AAHd8LSeiKMTEmGG8r0uF2uqcCQBWK7HzSk') // We s
 
 bot.start(ctx => ctx.reply(`
    Hi, I'm a simple bot
-`))
+`)
+  console.log("text"))
 
 bot.launch();
